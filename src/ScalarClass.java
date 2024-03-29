8826725e501fad863b2d1910555841d8480016ed
subra <subra@LAPTOP-R7QF5K7J>
2024-03-29 19:59:44 +0530

    Push the JRE project

---
+++ src/ScalarClass.java
@@ -0,0 +1,16 @@
+
+public final class ScalarClass {
+
+	 int num =10;
+
+	public void m1() {
+		System.out.println(this.num);
+	}
+	public void m2() {
+		
+	}
+	public static void main(String[] args) {
+		ScalarClass sc = new ScalarClass();
+		sc.m1();
+	}
+}
